c4eb770d0cff0fe44a7da0ca448d5670d6180853
Kirill Zaidulin <zaidulinkirill@gmail.com>
2022-12-30 16:46:38 +0300

    fix splash screen

--- template/App.tsx
+++ template/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
+import { hide } from 'react-native-bootsplash';
 
 export default function App() {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      hide();
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <SafeAreaView>
       <View>
         <Text>
-          Hello, world!
+          Hello, world
         </Text>
       </View>
     </SafeAreaView>
