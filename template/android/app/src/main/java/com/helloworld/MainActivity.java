f0508257c37a719a3c57ca02a1139c659f9f383b
Kirill Zaidulin <zaidulinkirill@gmail.com>
2022-12-30 12:43:16 +0300

    update android splash

--- template/android/app/src/main/java/com/helloworld/MainActivity.java
+++ template/android/app/src/main/java/com/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.helloworld;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +14,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "HelloWorld";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this);
+    super.onCreate(savedInstanceState);
   }
 
   /**
