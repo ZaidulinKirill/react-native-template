87e536d3b35ba219ac3e8194f3dfb19f39ab3be4
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-01-04 17:05:27 +0300

    stash

---
+++ template/src/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from '@kirz/react-native-toolkit';
+
+export default createTheme({
+  typography: {
+  },
+  fonts: {
+  },
+  colors: {
+  },
+});
---
+++ template/src/types.ts
@@ -0,0 +1,14 @@
+import theme from './theme';
+
+declare module '@kirz/react-native-toolkit' {
+  type ThemeTextVariants = typeof theme.typography;
+  export interface TextVariants extends ThemeTextVariants {}
+
+  type ThemeFontFamilies = typeof theme.fonts;
+  export interface FontFamilies extends ThemeFontFamilies {}
+
+  type ThemeColors = typeof theme.colors;
+  export interface Colors extends ThemeColors {}
+}
+
+export {};
